07e344be3c8f14c1bfd47cb51f26fa41df5f55c7
Егор <egor.avtyuhov@yandex.ru>
2025-10-19 23:28:09 +0300

    added cd and fixed ls

--- src/EasyPart.py
+++ src/EasyPart.py
@@ -2,19 +2,24 @@ import os
 from datetime import datetime
 
 
+
 def ls(abs_path, user_input):
     try:
-        path = user_input[-1] if user_input and user_input != ['-l'] else abs_path
-        user_input = [0] if not user_input else user_input
-        directory = []
-        if path[1:3] != ':\\':
-            path = abs_path + '\\' + path
-        directory = os.listdir(path)
+        if user_input == []:
+            directory = os.listdir(abs_path)
+            operation = None
+        else:
+            operation = user_input.pop(0) if user_input[0][0]=='-' else None
+            path = ' '.join(user_input) if user_input else abs_path
 
-        if user_input[0] == '-l':
+            if path[1:3] != ':\\':
+                path = abs_path + '\\' + path
+            directory = os.listdir(path)
+
+        if operation == '-l':
             for file in directory:
                 file_info = (f"Name: {file:30}  Size:{int(os.path.getsize(path + "\\" + file))//8:12}  Date: "
-                      f"{datetime.fromtimestamp(int(os.path.getctime(path + "\\" + file)))}  Permission: ")
+                      f"{datetime.fromtimestamp(int(os.path.getatime(path + "\\" + file)))}  Permission: ")
                 file_permission = f"{bin(os.stat(path + "\\" + file).st_mode)[-12:]}"
                 for i in range(len(file_permission)):
                     if file_permission[i] == '1':
@@ -27,11 +32,31 @@ def ls(abs_path, user_input):
                     else:
                         file_info += '-'
                 print(file_info)
-        else:
+        elif operation == None:
             for file in directory:
                 print(f"{file}")
-    except:
-        if len(user_input) <= 1:
-            exit(f'The directory "{user_input}" does not exist.')
         else:
-            exit(f'The directory "{user_input[1]}" does not exist or unknown option.')
+            print("Unknown operation")
+    except (PermissionError):
+        print("Permission error")
+    except (FileNotFoundError):
+        print("File not found")
+
+
+def cd(abs_path, user_input):
+    try:
+        if user_input == ['..']:
+            abs_path = '\\'.join(abs_path.split('\\')[:-1])
+            return abs_path
+        elif user_input == ['~']:
+            abs_path = os.path.expanduser('~')
+            return abs_path
+        else:
+            user_input = ' '.join(user_input)
+            if user_input[1:2] == ':':
+                return user_input
+            else:
+                return abs_path + "\\" + user_input
+
+    except:
+        exit("wtf")
--- src/main.py
+++ src/main.py
@@ -1,26 +1,40 @@
 import sys
 import os
 from commandList import commands
-from EasyPart import ls
+from EasyPart import ls, cd
 
 
 def main() -> None:
     print("Для завершения ввода напишите exit")
     print("Чтобы увидеть список команд напишите help")
+
     abs_path = os.path.abspath(__file__)[:-12]
+    print(abs_path+'\\ ', end='')
     for user_input in sys.stdin:
         user_input = user_input.split()
         user_command = user_input.pop(0)
-        if user_command not in commands:
-            exit("Unknown command")
 
-        if user_command == "exit":
-            break
-        elif user_command == "help":
-            print(commands)
-        elif user_command == "ls":
-            ls(abs_path, user_input)
+        match user_command:
+            case "help":
+                print(commands)
 
+            case "exit":
+                break
+
+            case "ls":
+                ls(abs_path, user_input)
+
+            case "cd":
+                abs_path = cd(abs_path, user_input)
+
+            case _:
+                exit("Unknown command")
+
+
+        if os.path.exists(abs_path):
+            print(abs_path, end=' ')
+        else:
+            exit("File or directory does not exist")
 
 
 if __name__ == "__main__":
